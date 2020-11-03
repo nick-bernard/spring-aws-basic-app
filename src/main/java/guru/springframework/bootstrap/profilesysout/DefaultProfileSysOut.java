dd536a3330ec4feaf0865262127089ba5cc8022f
Nicholas Bernard <nbernard@albany.edu>
2020-11-03 09:45:25 -0500

    More work with spring profiles and the properties file

--- src/main/java/guru/springframework/bootstrap/profilesysout/DefaultProfileSysOut.java
+++ src/main/java/guru/springframework/bootstrap/profilesysout/DefaultProfileSysOut.java
@@ -1,7 +1,10 @@
 package guru.springframework.bootstrap.profilesysout;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
 
 /**
  * Created by nick on Nov 3, 2020
@@ -13,7 +16,9 @@ import org.springframework.stereotype.Component;
 @Profile("default")
 public class DefaultProfileSysOut {
 
-    public DefaultProfileSysOut() {
+    @Autowired
+    public DefaultProfileSysOut(@Value("${guru.springframework.profile.message}")  String msg) {
         System.out.println("default!!!!!!");
+        System.out.println(msg);
     }
 }
